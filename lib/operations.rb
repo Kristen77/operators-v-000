262233f48f73a77dafb4811d160114c74a31ee47
Kristen Robertson <kristen77@gmail.com>
2018-09-21 00:21:30 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def unsafe?(speed)
   if speed < 60 || speed > 40 
     return true
@@ -10,6 +11,7 @@ end
 
 def not_safe?(speed)
 	speed < 60 || speed > 40 ? true : false
+	binding.pry
 end
 	
 
